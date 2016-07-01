e4ca00c3b39a9eff11bb7e1d43c030fa358af22d
chrisreed87 <chrisreed87@gmail.com>
2016-06-30 21:14:33 -0700

    initial upload

---
+++ quickbind.ps1
@@ -0,0 +1,12 @@
+﻿Write-Host `n
+Write-Host "The purpose of this script is to parse a block of text containing IPs and bind/unbind to the PRIMARY NIC"
+Write-Host `n
+Write-Host "PROCEED WITH CAUTION!"
+Write-Host `n
+Write-Host "Enter a string of IPs in this format 192.168.1.1,255.255.255.0 (ipaddress, subnet)"
+$Blockpath = Read-Host -Prompt "Enter IP(s):"
+Write-Host `n
+$Selection = Read-Host -Prompt "What would you like to do, add or remove?"
+Write-Host `n
+If ($Selection -eq "add") {Write-Host "Binding IPs to Primary NIC"}
+If ($Selection -eq "remove") {Write-Host "Removing IPs from Primary NIC"}
