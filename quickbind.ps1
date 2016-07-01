f03561f3d995a8667f4393db05ca30eb1fd1c122
chrisreed87 <chrisreed87@gmail.com>
2016-07-01 13:06:16 -0700

    updated quickbind w/ panic option

--- quickbind.ps1
+++ quickbind.ps1
@@ -1,12 +1,58 @@
-﻿Write-Host `n
-Write-Host "The purpose of this script is to parse a block of text containing IPs and bind/unbind to the PRIMARY NIC"
+Clear-Host
+Write-Host "The purpose of this script is to quickly bind/unbind to the PRIMARY NIC `nthe PANIC option removes EVERY IP that is not the source/primary"
 Write-Host `n
-Write-Host "PROCEED WITH CAUTION!"
+Write-Host "PROCEED WITH CAUTION & CHOOSE WISELY!"
 Write-Host `n
-Write-Host "Enter a string of IPs in this format 192.168.1.1,255.255.255.0 (ipaddress, subnet)"
-$Blockpath = Read-Host -Prompt "Enter IP(s):"
+
+#-----------User Prompt----------------------------#
+
+$Selection = Read-Host -Prompt "What would you like to do? add, remove, or panic?"
+
 Write-Host `n
-$Selection = Read-Host -Prompt "What would you like to do, add or remove?"
-Write-Host `n
-If ($Selection -eq "add") {Write-Host "Binding IPs to Primary NIC"}
-If ($Selection -eq "remove") {Write-Host "Removing IPs from Primary NIC"}
+Write-Host "If you selected panic, just hit ENTER at IP selection`nOtherwise add an IP and hit ENTER after each one, hit ENTER again when finished"
+
+#------------Variables -----------------------------#
+
+$newipaddress = @()
+do {
+ $input = (Read-Host "Enter the IP address")
+ if ($input -ne '') {$newipaddress += $input}
+}
+until ($input -eq '')
+
+$secondaryIPs = Get-NetIPAddress -InterfaceAlias Primary -SkipAsSource $True -ErrorAction SilentlyContinue |  foreach { $_.IPAddress }
+
+#--------------Commands------------------------------#
+
+#If Selection is to Add
+If ($Selection -eq "add") {Write-Host "Binding IPs to Primary NIC";
+$IPs = @($newipaddress) |`
+   Foreach-object {
+   netsh Int IPv4 Add Address Primary $_ SkipAsSource=True Mask=255.255.0.0
+   write-host "$_ Added $?"
+}
+}
+
+#If Selection is to Remove
+If ($Selection -eq "remove") {Write-Host "Removing IPs from Primary NIC";
+$IPs = @($newipaddress) |`
+   Foreach-object {
+   netsh Int IPv4 Delete Address Primary $_
+   write-host "$_ Removed $?"
+}
+}
+
+#If Selection is to Panic
+if ($Selection -eq "panic") {$Confirmation=Read-Host -Prompt "Are you sure? Do you know what this means? Y or N"}
+
+if ($Confirmation -eq "Y") { 
+$panic= @($secondaryIPs) | `
+    Foreach-object {
+    netsh Int IPv4 Delete Address Primary $_
+    write-host "$_ Removed $?"
+}
+}
+
+if ($Confirmation -ne "Y") {Write-Host "Have a nice life"}
+
+#END
